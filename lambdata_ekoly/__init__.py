4711781245b9e0c2ab37e9fc2291943b29375ec4
ekoly <ecowley@protonmail.com>
2020-01-13 13:34:47 -0500

    df utils

---
+++ lambdata_ekoly/__init__.py
@@ -0,0 +1,9 @@
+"""
+lamdata - a collection of data science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROES = pd.DataFrame(np.zeroes(50))
