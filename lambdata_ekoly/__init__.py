32a6702f6c9ebf6a2f8338eb0d7287bcd978bab5
ekoly <ecowley@protonmail.com>
2020-01-13 16:49:32 -0500

    zeroes -> zeros

--- lambdata_ekoly/__init__.py
+++ lambdata_ekoly/__init__.py
@@ -5,5 +5,5 @@ lamdata - a collection of data science helper functions
 import pandas as pd
 import numpy as np
 
-ONES = pd.DataFrame(np.ones(10))
-ZEROES = pd.DataFrame(np.zeroes(50))
+ONES = pd.DataFrame(np.ones(50))
+ZEROES = pd.DataFrame(np.zeros(50))
