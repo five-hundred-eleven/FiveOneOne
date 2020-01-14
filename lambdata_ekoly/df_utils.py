a0c083205b98da707ffb60dff82cf1d07ccdd664
ekoly <ecowley@protonmail.com>
2020-01-14 15:52:15 -0500

    added docstrings

--- lambdata_ekoly/df_utils.py
+++ lambdata_ekoly/df_utils.py
@@ -19,6 +19,7 @@ def train_validate_test_split(df, train_size=0.6):
         @type train_size: float
         @param train_size: The size of the training set. The val and test sets
             will be split 50:50 among the rest
+        @returns: tuple of three dataframes
     """
     train, val_test = train_test_split(df, train_size=train_size)
     val, test = train_test_split(val_test, train_size=0.5)
@@ -27,20 +28,40 @@ def train_validate_test_split(df, train_size=0.6):
 
 
 def confidenceInterval(data, confidence=0.95):
-  data = np.array(data)
-  data = data[~np.isnan(data)]
-  mean = np.mean(data)
-  n = len(data)
-  stderr = np.std(data, ddof=1)/np.sqrt(n)
-  moe = stderr * t.ppf((1.+confidence)/2., n-1)
+    """
+        Returns the confidence interval and mean for the data within the given
+        {confidence}.
 
-  ci1, ci2 = t.interval(confidence, n-1, loc=mean, scale=stderr)
-  return (ci1, mean, ci2)
+        @type data: DataFrame
+        @type confidence: float
+
+        @returns: tuple of 3 floats representing the left confidence interval,
+            the median, and the right confidence interval for the data.
+    """
+    data = np.array(data)
+    data = data[~np.isnan(data)]
+    mean = np.mean(data)
+    n = len(data)
+    stderr = np.std(data, ddof=1)/np.sqrt(n)
+    moe = stderr * t.ppf((1.+confidence)/2., n-1)
+
+    ci1, ci2 = t.interval(confidence, n-1, loc=mean, scale=stderr)
+    return (ci1, mean, ci2)
 
 
 def bayesConfidenceInterval(data, confidence=0.95):
-  data = np.array(data)
-  data = data[~np.isnan(data)]
-  means, _, _ = bayes_mvs(data)
-  mean, moe = means
-  return (moe[0], mean, moe[1])
+    """
+        Returns the confidence interval and mean for the data within the given
+        {confidence}.
+
+        @type data: DataFrame
+        @type confidence: float
+
+        @returns: tuple of 3 floats representing the left confidence interval,
+            the median, and the right confidence interval for the data.
+    """
+    data = np.array(data)
+    data = data[~np.isnan(data)]
+    means, _, _ = bayes_mvs(data)
+    mean, moe = means
+    return (moe[0], mean, moe[1])
